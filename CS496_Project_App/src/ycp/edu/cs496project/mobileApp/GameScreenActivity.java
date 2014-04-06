1c132bc79fa828d6ea1ac07e2816a0b3bbf6703b
Chris Flinchbaugh <cflinch2@ycp.edu>
2014-04-06 16:33:07 -0400

    Edited image array, minutely.

--- CS496_Project_App/src/ycp/edu/cs496project/mobileApp/GameScreenActivity.java
+++ CS496_Project_App/src/ycp/edu/cs496project/mobileApp/GameScreenActivity.java
@@ -31,8 +31,7 @@ public class GameScreenActivity extends Activity {
 	
 	private GridView gameGrid; //the game grid
 	Integer[] imageArray = {R.drawable.sample_0, R.drawable.sample_1, R.drawable.sample_2, R.drawable.sample_3,
-							R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_5, R.drawable.sample_6,
-							R.drawable.sample_7};
+							R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_6, R.drawable.sample_7};
 	
 	ImageButton[] imgButtonArr = new ImageButton[imageArray.length];
 	
